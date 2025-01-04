51b095b94965dcddf25bd5237e7a82793305e65e
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-01-04 16:40:50 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,15 @@
+// alert(3 + 3)
+
+// number => 2 to the power 53
+// BigInt
+// String=> ""
+// Boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// object
+
+console.log(typeof "Aman Kaushik"); // undefined
+console.log(typeof null); // object
+
