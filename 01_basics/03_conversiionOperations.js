e3c931e19fa079c410f331a11995a4cd8c7c41bd
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-01-04 16:41:17 +0000

    datatypes conversions

---
+++ 01_basics/03_conversiionOperations.js
@@ -0,0 +1,24 @@
+let score = true
+
+console.log(typeof score);
+
+let valueinNumber = Number(score)
+console.log(typeof(valueinNumber));
+console.log(valueinNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// null => 0
+// undefined => NaN
+// true => 1
+// false => 0
+
+let isLoggedIn = 1
+
+let BooleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(BooleanLoggedIn);
+
+// 1 => true; 0 => false;
+// "" => false
+// "string" => true
