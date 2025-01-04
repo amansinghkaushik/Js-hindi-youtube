819ab13f1091696ac638a49bc1f59a9e0a3f800e
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-01-04 16:40:31 +0000

    variables

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 144553
+let accountEmail = "amansinghkaushik@gmail.com"
+var accountPassword = "12345"
+accountCity = "Gorakhpur"
+let accountState;
+
+//accountId = 2 //Not allowed
+
+accountEmail = "aman@gmail.com"
+accountPassword = "212121"
+accountCity = "Delhi"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity , accountState]);
