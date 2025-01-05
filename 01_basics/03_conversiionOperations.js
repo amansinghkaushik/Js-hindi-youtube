aa544684ad3aaee591c29ada6bf9c1e65602dbfc
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-01-05 11:44:26 +0000

    datatype conversions

--- 01_basics/03_conversiionOperations.js
+++ 01_basics/03_conversiionOperations.js
@@ -1,10 +1,10 @@
 let score = true
 
-console.log(typeof score);
+//console.log(typeof score);
 
 let valueinNumber = Number(score)
-console.log(typeof(valueinNumber));
-console.log(valueinNumber);
+//console.log(typeof(valueinNumber));
+//console.log(valueinNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -17,8 +17,26 @@ let isLoggedIn = 1
 
 let BooleanLoggedIn = Boolean(isLoggedIn)
 
-console.log(BooleanLoggedIn);
+// console.log(BooleanLoggedIn);
 
 // 1 => true; 0 => false;
 // "" => false
 // "string" => true
+
+// ***************************Operations*******************************
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+let str1 = "Aman"
+let str2 = " Kaushik"
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
+
+
