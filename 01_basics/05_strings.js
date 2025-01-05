300a89f1d6dbed9fc4844325c3b1c53f7d4556b1
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-01-05 12:28:48 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Aman"
+const repoCount  =  50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`);
+
+const gameName = new String('AmanKaushik')
+
+console.log(gameName[0]);
+
+console.log(gameName.length);
+console.log(gameName.toUpperCase());
+console.log(gameName.charAt(4));
+console.log(gameName.indexOf("k"));
+
+const newString = "   Aman   "
+
+console.log(newString);
+console.log(newString.trim());
+
+const url = "http://aman.com/aman%20kaushik"
+
+console.log(url.replace("%20", "-"));
+console.log(url.includes("aman"));
+
+const newName = "Aman-Singh-Kaushik"
+console.log(newName.split("-"));
