f500825f6815e7760fdf8aa313688e6d6fcf0cd7
Aman Kaushik <amansinghkaushik8@gmail.com>
2025-02-05 22:27:26 +0000

    2 projects done

---
+++ 07_projects/05_changeofColors/script.js
@@ -0,0 +1,29 @@
+//genrate a rando color
+const randomColor = function(){
+    const hex = "012356789ABCDEF"
+  
+    let color = "#"
+    for(let i = 0; i<6; i++){
+      color += hex[Math.floor(Math.random()* 16)]
+    }
+    return color;
+  };
+  
+  let intervalId
+  
+  const startChangingColor = function(){
+    intervalId = setInterval(changeBgColor, 1000);
+  
+    function changeBgColor(){
+      document.body.style.backgroundColor = randomColor()
+    }
+  }
+  
+  const stopChangingColor = function(){
+    clearInterval(intervalId)
+    intervalId = null
+  }
+  
+  document.querySelector("#start").addEventListener("click", startChangingColor)
+  
+  document.querySelector("#start").addEventListener("click", stopChangingColor)
